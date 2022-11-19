255d33413acc1ad4f7643a5324abace181233a0d
isksha <101995733+isksha@users.noreply.github.com>
2022-11-18 20:47:24 -0500

    milestone3

---
+++ milestone3/database_ddl.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE SPOTIFY_DB;
+USE SPOTIFY_DB;
+
+CREATE TABLE Songs (
+    id VARCHAR(50),
+    name VARCHAR(255),
+    artists VARCHAR(255),
+    album VARCHAR(255),
+    explicit TINYINT(1),
+    danceability DECIMAL(4,3),
+    energy DECIMAL(4,3),
+    song_key INT(32),
+    loudness DECIMAL(4,3),
+    speechiness DECIMAL(5,4),
+    acousticness DECIMAL(5,4),
+    instrumentalness DECIMAL(7,6),
+    liveness DECIMAL(5,4),
+    valence DECIMAL(4,3),
+    tempo DECIMAL(6,3),
+    duration_ms INT(32),
+    year INT(32),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Charting (
+    song_rank INT(32),
+    song_id VARCHAR(50),
+    region VARCHAR(30),
+    chart VARCHAR(7),
+    trend VARCHAR(9),
+    streams INT(32),
+    chart_date DATE,
+    PRIMARY KEY (song_id)
+);
+
+CREATE TABLE Users (
+    user_id INT(32),
+    email VARCHAR(100),
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Saved_Songs (
+    user_id INT(32),
+    song_id VARCHAR(50),
+    PRIMARY KEY (user_id, song_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (song_id) REFERENCES Songs(id)
+);
