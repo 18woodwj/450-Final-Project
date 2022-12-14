013a43fe1e996633a1240d6f662a01d2e0a9e8fe
Cynthia Yee <cynthyee@sas.upenn.edu>
2022-12-14 13:05:44 -0500

    Update cynthia_queries.sql

--- cynthia_queries.sql
+++ cynthia_queries.sql
@@ -6,25 +6,30 @@ VALUES (user_id, "email", "region");
 with more than 20 songs*/
 WITH dance AS
         (SELECT avg(S.danceability) AS danceavg
-        FROM Saved_Songs SS JOIN Songs S ON SS.song_id = S.id
+        FROM Saved_Songs SS JOIN Songs S ON SS.song_id = S.id JOIN Charting C on SS.song_id = C.song_id
         WHERE user_id = 2),
     energy AS
         (SELECT avg(S.energy) AS energyavg
-        FROM Saved_Songs SS JOIN Songs S ON SS.song_id = S.id
+        FROM Saved_Songs SS JOIN Songs S ON SS.song_id = S.id JOIN Charting C on SS.song_id = C.song_id
         WHERE user_id = 2),
     acoustic AS
         (SELECT avg(S.acousticness) AS acavg
-        FROM Saved_Songs SS JOIN Songs S ON SS.song_id = S.id
+        FROM Saved_Songs SS JOIN Songs S ON SS.song_id = S.id JOIN Charting C on SS.song_id = C.song_id
         WHERE user_id = 2),
     instrument AS
         (SELECT avg(S.instrumentalness) AS inavg
-        FROM Saved_Songs SS JOIN Songs S ON SS.song_id = S.id
+        FROM Saved_Songs SS JOIN Songs S ON SS.song_id = S.id JOIN Charting C on SS.song_id = C.song_id
         WHERE user_id = 2)
 SELECT DISTINCT(S.name), S.artists
 FROM Charting C JOIN Songs S ON S.id = C.song_id, dance, energy, acoustic, instrument
 WHERE C.region = "United States" AND
-      (dance.danceavg + 0.3 <= S.danceability OR dance.danceavg - 0.3 >= S.danceability) AND
-      (energy.energyavg + 0.2 <= S.energy OR energy.energyavg - 0.2 >= S.energy) AND
-      (acoustic.acavg + 0.2 <= S.acousticness OR acoustic.acavg - 0.2 >= S.acousticness) AND
-      (instrument.inavg + 0.2 <= S.instrumentalness OR instrument.inavg - 0.2 >= S.instrumentalness)
+      (dance.danceavg + 0.1 <= S.danceability OR dance.danceavg - 0.1 >= S.danceability) AND
+      (energy.energyavg + 0.1 <= S.energy OR energy.energyavg - 0.1 >= S.energy) AND
+      (acoustic.acavg + 0.1 <= S.acousticness OR acoustic.acavg - 0.1 >= S.acousticness) AND
+      (instrument.inavg + 0.1 <= S.instrumentalness OR instrument.inavg - 0.1 >= S.instrumentalness)
 LIMIT 10
+
+SELECT DISTINCT(S.name), S.artists
+FROM Songs S JOIN Charting C on S.id = C.song_id
+WHERE C.region = "United States"
+LIMIT 100
